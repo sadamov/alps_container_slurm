36c31e483ca56b77a4de5caadea13d0036e7a8a0
sadamov <kode@mailbox.org>
2025-01-27 11:07:07 +0100

    retrieve LSM from cosmo-archive

---
+++ land_sea_mask/retrieve_frland.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import xarray as xr
+
+ds = xr.open_dataset(
+    "./ceffsurf000_000",
+    engine="cfgrib",
+    backend_kwargs={
+        "filter_by_keys": {"typeOfLevel": "surface", "stepType": "instant"}
+    },
+)
+
+plt.figure()
+ds.lsm.plot(add_colorbar=False)
+plt.title("Land-sea mask")
+plt.savefig("land_sea_mask.png")
+plt.close()
+
+ds.lsm.to_zarr("land_sea_mask.zarr", mode="w")
